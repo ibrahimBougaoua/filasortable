7a6b3a026a009e9d4cc10e012d249954b16bf67d
Ibrahim Bougaoua <41789518+ibrahimBougaoua@users.noreply.github.com>
2024-09-18 17:06:44 +0100

    First Commit

--- src/Commands/FilaSortableCommand.php
+++ src/Commands/FilaSortableCommand.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton\Commands;
+namespace IbrahimBougaoua\FilaSortable\Commands;
 
 use Illuminate\Console\Command;
 
-class SkeletonCommand extends Command
+class FilaSortableCommand extends Command
 {
-    public $signature = 'skeleton';
+    public $signature = 'filasortable';
 
     public $description = 'My command';
 
--- src/FilaSortableServiceProvider.php
+++ src/FilaSortableServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace IbrahimBougaoua\FilaSortable;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use IbrahimBougaoua\FilaSortable\Commands\FilaSortableCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class FilaSortableServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +16,10 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
+            ->name('filasortable')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_migration_table_name_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->hasMigration('create_filasortable_table')
+            ->hasCommand(FilaSortableCommand::class);
     }
 }
